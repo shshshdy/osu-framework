a166eaa95b41cab19c3043ef537ff9469e61fe05
ssd <shshshdy@163.com>
2024-05-12 14:21:59 +0800

    sample

---
+++ SampleGame/MyCircle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Input.Events;
+
+namespace SampleGame
+{
+    public partial class MyCircle : Circle
+    {
+        public Action? Click { get; set; }
+        public Action? RightClick { get; set; }
+        public Action? DoubleClick { get; set; }
+
+        protected override bool OnClick(ClickEvent e)
+        {
+            if (e.Button == osuTK.Input.MouseButton.Left)
+            {
+                Click?.Invoke();
+            }
+            else
+            {
+                RightClick?.Invoke();
+            }
+            return base.OnClick(e);
+        }
+        protected override bool OnDoubleClick(DoubleClickEvent e)
+        {
+            DoubleClick?.Invoke();
+            return base.OnDoubleClick(e);
+        }
+    }
+}
+
--- SampleGame/SampleGameGame.cs
+++ SampleGame/SampleGameGame.cs
@@ -7,6 +7,10 @@ using osuTK;
 using osuTK.Graphics;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Allocation;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Textures;
+using System.IO;
+using osu.Framework.Graphics.UserInterface;
 
 namespace SampleGame
 {
@@ -14,18 +18,58 @@ namespace SampleGame
     {
         private Box box = null!;
 
+        private MyCircle circle = null!;
+        private Texture texture = null!;
+        private Button btn = null!;
         [BackgroundDependencyLoader]
         private void load()
         {
+            using (var stream = File.OpenRead("logo.png"))
+            {
+                texture = Texture.FromStream(Host.Renderer, stream)!;
+            }
             Add(box = new Box
             {
+                Name = "box",
                 Anchor = Anchor.Centre,
                 Origin = Anchor.Centre,
-                Size = new Vector2(150, 150),
-                Colour = Color4.Tomato
+                Size = new Vector2(350, 350),
+                Colour = Color4.Tomato,
             });
+            Add(new Sprite
+            {
+                Name = "Sprite",
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Texture = texture,
+                Colour = Colour4.Blue,
+                Blending = BlendingParameters.Additive
+            });
+            Add(circle = new MyCircle
+            {
+                Name = "circle",
+                Size = new Vector2(150, 150),
+                Colour = Colour4.Red,
+                BorderColour = Color4.Blue,
+                Position = new Vector2(120, 120),
+                Child = new Sprite
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Texture = texture,
+                },
+            });
+            circle.Click = () =>
+            {
+                i++;
+                circle.Colour = i % 2 == 0 ? Colour4.Yellow : Colour4.Red;
+            };
+            if (RuntimeInfo.IsMobile)
+            {
+                circle.Scale = new Vector2(2, 2);
+            }
         }
-
+        private int i = 0;
         protected override void Update()
         {
             base.Update();
